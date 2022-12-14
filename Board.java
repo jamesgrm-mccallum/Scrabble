5f10cae40676ff9016db8e9cd032a1e8f1cc1254
xshadow989x <jamesgrm081@gmail.com>
2022-12-14 14:41:29 -0500

    asdasd

--- Board.java
+++ Board.java
@@ -116,18 +116,47 @@ class Board {
         return gameBoard;
     }
 
+    @Override
     public String toString(){
+        String accum = "";
         String row_accum = "";
-        String cell;
-        for (int r = 0; r < 14; r++){
-            for (int c = 0; c < 14; c++){
-                cell = String.format("|%s|", gameBoard[c][r].toString());
-                row_accum += cell;
-            }
+        String cell = "";
+        for (int h = 0; h < 15; h++){
+            char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+            cell = ""; 
+            cell += String.format("| %s ", alphabet[h]);
+            row_accum += cell;
+        
         }
-        return row_accum;
+
+        
+        accum += "   " + row_accum + "|" + "\n";
+        for (int r = 0; r < 15; r++){
+            row_accum = "";
+            row_accum += "---------------------------------------------------------------\n";
+            for (int c = 0; c < 15; c++){
+                if (c == 0){
+                    row_accum += String.valueOf(r + 1);
+                    if (r < 9){
+                        row_accum += " ";
+                    }
+                }
+                
+                cell = String.format(" |%s ", " ");
+                row_accum += cell;
+                if (c == 14){
+                    row_accum += " |";
+                }
+                
+            }
+            row_accum += "\n";
+            accum += row_accum;
+        }
+        
+        return accum;
     }
     public static void main(String[] args) {
+        Board gameBoard = new Board();
         gameBoard.toString();
     }
 }
