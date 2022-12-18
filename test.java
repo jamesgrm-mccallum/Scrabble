6ad39888e4c80502e913f3d79c71c7594df604e9
xshadow989x <jamesgrm081@gmail.com>
2022-12-18 13:00:10 -0500

    NEARLY FINISHED ON INPUT VALIDATION

--- test.java
+++ test.java
@@ -2,17 +2,17 @@ import java.util.*;
 
 public class test {
     public static void main(String[] args) {
-        if ("abcc".contains("abc")){
-            System.out.println("true");
-        }
-        ArrayList<String> strings = new ArrayList<String>();
-        strings.add("h");
-        strings.add("e");
-        strings.add("l");
-        strings.add("l");
-        strings.add("o");
-        String listString = String.join("", strings);
-        System.out.println(listString);
-        System.out.println(listString.length());
+    if ("abcc".contains("abc")){
+        System.out.println("true");
     }
-}
+    ArrayList<String> strings = new ArrayList<String>();
+    strings.add("h");
+    strings.add("e");
+    strings.add("l");
+    strings.add("l");
+    strings.add("o");
+    String listString = String.join("", strings);
+    System.out.println(listString);
+    System.out.println(listString.length());
+    }
+    }
